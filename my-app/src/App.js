3b8fc312e880ab62d2a6c8eef497767f00d5ace2
Cogita (David) <ldpmendes@gmail.com>
2023-01-13 21:21:53 +0000

    experiments

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,6 @@
-import { useState } from "react";
-import styled, { css } from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { createGlobalStyle } from "styled-components";
-
-const StyledButton = styled.button`
-  background: white;
-  border: 2px solid black;
-  border-radius: 8px;
-  background-color: transparent;
-  width: 40px;
-  height: 30px;
-`;
-
-const StyledTotal = styled.span`
-  padding: 6px;
-  color: ${(props) => `${props.color}`};
-  border: ${(props) => `2px solid ${props.border}`};
-
-  ${({ count }) => {
-    if (count)
-      return css`
-        border-top: 2px solid red;
-        border-bottom: 2px solid red;
-      `;
-    else
-      return css`
-        border-left: 2px solid blue;
-        border-right: 2px solid blue;
-      `;
-  }}
-`;
+import background from "./back.gif";
 
 const GlobalStyle = createGlobalStyle`
 * {
@@ -37,25 +9,108 @@ const GlobalStyle = createGlobalStyle`
   font-weight: bolder;
   font-size: large;
   text-align: center;
-}
+  }
+`;
+
+const Crystal = keyframes`
+0% {
+  rotate: 0deg;
+  background: linear-gradient(217deg, rgba(20,0,200,.8), rgba(0,0,0,0) 70.71%),
+  linear-gradient(127deg, rgba(255,255,255,1), rgba(80,80,255,0.2) 70.71%),
+  linear-gradient(336deg, rgba(70,110,255,.8), rgba(50,50,255,0.4) 70.71%);
+  }
+100% {
+  rotate: 360deg;
+  background: linear-gradient(217deg, rgba(20,0,200,.8), rgba(0,0,0,0) 70.71%),
+  linear-gradient(127deg, rgba(255,255,255,1), rgba(80,80,255,0.2) 70.71%),
+  linear-gradient(336deg, rgba(70,110,255,.8), rgba(50,50,255,0.4) 70.71%);
+  }
+`;
+
+const Light = keyframes`
+0% {
+  scale: 100%;
+  }
+  50% {
+  scale: 150%;
+  }
+  100% {
+  scale: 100%;
+  }
+`;
+
+const FlexWrapper = styled.div`
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  min-width: 100vw;
+  min-height: 15vh;
+  position: fixed;
+  @media only screen and (max-height: 800px){
+    min-height: 20vh;
+  }
+`;
+
+const Character = styled.div`
+  margin: auto;
+  width: 350px;
+  height: 700px;
+  background-image: url(${background});
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+
+const CrystalBall = styled.div`
+  margin: auto;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background-color: cyan;
+  animation-name: ${Crystal};
+  animation-duration: 2s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+`;
+
+const LightEffect = styled.div`
+  margin: auto;
+  width: 400px;
+  height: 400px;
+  margin-bottom: -200px;
+  border-radius: 50%;
+  background-color: cyan ;
+  opacity: 0.2;
+  background-image: none;
+  animation-name: ${Light};
+  animation-duration: 4s;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-in-out;
+`;
+
+const HighLight = styled.div`
+  margin: auto;
+  width: 12px;
+  height: 12px;
+  margin-top: 50px;
+  margin-bottom: -220px;
+  padding-left: 20px;
+  border-radius: 50%;
+  background-color: white;
+  opacity: 0.6;
+  background-image: none;
+  z-index: 1;
 `;
 
 export default function App() {
-  const [count, setCount] = useState(0);
   return (
     <>
       <GlobalStyle />
-      <StyledButton onClick={() => setCount(count - 1)}>-</StyledButton>{" "}
-      <StyledButton onClick={() => setCount(count + 1)}>+</StyledButton>
-      <p />
-      total:{" "}
-      <StyledTotal
-        border="yellow"
-        count={count % 2 === 0}
-        color={count < 0 ? "red" : "blue"}
-      >
-        {count}
-      </StyledTotal>
+      <FlexWrapper>
+        <HighLight />
+        <LightEffect/>
+        <CrystalBall />
+        <Character />
+      </FlexWrapper>
     </>
   );
 }
